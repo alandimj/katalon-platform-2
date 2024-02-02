0df804bc95641356366ca0cfa7455400eb91b3eb
alandimarsyajaya <alandimarsyajaya@gmail.com>
2024-02-02 15:14:31 +0700

    .

---
+++ Object Repository/Wikipedia/Page_Wikipedia/btn_search.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_search</name>
+   <tag></tag>
+   <elementGuidId>654d3508-ec25-4210-8421-0d06ce228128</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;search-form&quot;]/fieldset/button/i</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
